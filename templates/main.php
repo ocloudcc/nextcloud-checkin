22a8af7faef9fedc13e60c395a289fe08c950367
ocloudcc <ocloudcc@outlook.com>
2024-05-27 00:09:18 +0800

    Create main.php

---
+++ templates/main.php
@@ -0,0 +1,12 @@
+<?php
+style('checkin', 'style');
+script('checkin', 'main');
+?>
+
+<div id="app">
+    <h1>Daily Check-in</h1>
+    <form id="checkin-form">
+        <button type="submit">Check-in</button>
+    </form>
+    <div id="message"></div>
+</div>

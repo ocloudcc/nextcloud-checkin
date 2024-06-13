c82353a0c44085668e53056699d054d430e57b04
ocloudcc <ocloudcc@outlook.com>
2024-06-13 22:22:28 +0800

    Create zh_CN.js

---
+++ l10n/zh_CN.js
@@ -0,0 +1,9 @@
+OC.L10N.register(
+    "checkin",
+    {
+    "Checkin" : "签到",
+    "Check-in" : "签到",
+    "Daily Check-in" : "每日签到",
+    "{author}, {timestamp}" : "{author}, {timestamp}"
+},
+"nplurals=1; plural=0;");

8df6ee33c5659a84c00b02ef6655cf17b366ced7
ocloudcc <ocloudcc@outlook.com>
2024-06-13 22:10:29 +0800

    Update main.php

--- templates/main.php
+++ templates/main.php
@@ -4,9 +4,9 @@ script('checkin', 'main');
 ?>
 
 <div id="app">
-    <h1>Daily Check-in</h1>
+    <h1 id="Daily_Check-in">Daily Check-in</h1>
     <form id="checkin-form">
-        <button type="submit">Check-in</button>
+        <button type="submit" id="Check-in">Check-in</button>
     </form>
     <div id="message"></div>
 </div>

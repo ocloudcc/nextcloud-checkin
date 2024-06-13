71ad204daa0f86e5b6f530cbae64605d6694e34d
ocloudcc <ocloudcc@outlook.com>
2024-06-13 22:04:45 +0800

    Update Application.php

--- lib/Application.php
+++ lib/Application.php
@@ -2,9 +2,17 @@
 namespace OCA\Checkin\AppInfo;
 
 use OCP\AppFramework\App;
+use OCP\IL10N;
 
 class Application extends App {
     public function __construct(array $urlParams = []) {
         parent::__construct('checkin', $urlParams);
+
+        $container = $this->getContainer();
+
+        $container->registerService('L10N', function($c) {
+            return $c->query(IL10N::class)->get('checkin');
+        });
+        
     }
 }

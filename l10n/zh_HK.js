b22e26b7b4d0960a6a4a793a855e9bde8cf195d6
ocloudcc <ocloudcc@outlook.com>
2024-06-13 22:23:13 +0800

    Add files via upload

---
+++ l10n/zh_HK.js
@@ -0,0 +1,9 @@
+OC.L10N.register(
+    "checkin",
+    {
+    "Checkin" : "簽到",
+    "Check-in" : "簽到",
+    "Daily Check-in" : "每日簽到",
+    "{author}, {timestamp}" : "{author}, {timestamp}"
+},
+"nplurals=1; plural=0;");

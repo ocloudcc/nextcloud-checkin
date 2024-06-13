deb2c66620483bf98998b102eed3718e934a0ec2
ocloudcc <ocloudcc@outlook.com>
2024-06-13 22:24:27 +0800

    Update user_checkins.sql

--- user_checkins.sql
+++ user_checkins.sql
@@ -1,5 +1,7 @@
 CREATE TABLE oc_user_checkins (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    uid VARCHAR(64) NOT NULL,
-    checkin_count INT DEFAULT 0
+    user_id VARCHAR(64) NOT NULL,
+    checkin_date DATE NOT NULL,
+    extra_space TEXT DEFAULT NULL,
+    total_extra_space TEXT DEFAULT NULL
 );
